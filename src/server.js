32a26d8efaa1556afdef6670c80edde66b8ce193
Anuskhancoding <147386838+Anuskhancoding@users.noreply.github.com>
2025-08-20 21:15:15 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,34 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static(path.join(__dirname, '../public')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
+
+// // Start the server and store the server instance
+// const server = app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+// // Export both app and server
+// module.exports = { app, server };
+
+
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
-
+// Your routes and middleware
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile('index.html', { root: __dirname });
 });
 
-// Start the server and store the server instance
-const server = app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
-
-// Export both app and server
-module.exports = { app, server };
